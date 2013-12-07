93bb06cee135b09a9e2943e62d540b60fb484751
withparadox2 <withparadox2@gmail.com>
2013-12-07 18:22:18 +0800

    添加强制刷新，并记录ip

--- src/com/withparadox2/autoedu/AutoEduActivity.java
+++ src/com/withparadox2/autoedu/AutoEduActivity.java
@@ -1,9 +1,11 @@
 package com.withparadox2.autoedu;
 
-import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
@@ -12,87 +14,95 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AutoEduActivity extends Activity {
-    /** Called when the activity is first created. */
+	/** Called when the activity is first created. */
 	public final static int NET_OK = 0; 
 	public final static int START_LOGIN = 1; 
 	public final static int LOGIN_SUCCESSED = 2; 
 	public final static int LOGIN_FAILED = 3;
-	
+
+	public final static int FORCE_LOGIN = 0;
+	public final static int NORMAL_LOGIN = 1;
+
+	private final static String SP_NAME = "temp_data";
+	public final static String SSID = "ssid";
+	public final static String WLANACNAME = "wlanacname";
+	public final static String WLANUSERIP = "wlanuserip";
+
 	private Context context;
-	
+
 	private TextView messageText;
 	private LoginInThread loginInThread;
 	private MyHandler myHandler;
-	
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        messageText = (TextView)findViewById(R.id.message_text);
-        context = this;
-        myHandler = new MyHandler(Looper.myLooper());
-      
-    }
+	private Button refreshButton;
 
 
-	/**
-	 * 访问百度，通过是否重定向来判断网络有没有连接，重定向则表明网络没有连接...
-	 */
-	private boolean isNetConnected() {  
-		if(LoginInThread.getLocationHeader()==null){
-			System.out.println("asdfasdf");
-		}else{
-			System.out.println("vvvvvvvv");
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		messageText = (TextView)findViewById(R.id.message_text);
+		refreshButton = (Button)findViewById(R.id.force_refresh_button);
+		refreshButton.setOnClickListener(new OnButtonClickListener());
+		context = this;
+		myHandler = new MyHandler(Looper.myLooper());
+		if(isStartLogin()){
+			loginInThread = new LoginInThread(myHandler, AutoEduActivity.this, NORMAL_LOGIN);
+			loginInThread.start();
 		}
-		return LoginInThread.getLocationHeader()==null;
-    }
-	
+
+	}
+
+	private class OnButtonClickListener implements OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			if(isStartLogin()){
+				loginInThread = new LoginInThread(myHandler, AutoEduActivity.this, FORCE_LOGIN);
+				loginInThread.start();
+			}
+		}
+	}
+
+
 	public boolean isWifiConnected(Context context) { 
 		if (context != null) { 
 			ConnectivityManager mConnectivityManager = (ConnectivityManager) context 
-		.getSystemService(Context.CONNECTIVITY_SERVICE); 
+					.getSystemService(Context.CONNECTIVITY_SERVICE); 
 			NetworkInfo mWiFiNetworkInfo = mConnectivityManager 
-		.getNetworkInfo(ConnectivityManager.TYPE_WIFI); 
+					.getNetworkInfo(ConnectivityManager.TYPE_WIFI); 
 			if (mWiFiNetworkInfo != null) { 
 				return mWiFiNetworkInfo.isAvailable(); 
 			} 
 		} 
 		return false; 
 	}
-    
+
 	private boolean isConnectToEdu(){
 		WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
 		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-//		messageText.setText(wifiInfo.getSSID());
 		if(wifiInfo != null && wifiInfo.getSSID().equals("CMCC-EDU")){
 			return true;
 		}else{
 			return false;
 		}
 	}
-    
+
 	private boolean isStartLogin(){
-		boolean result = false;
-		if(isWifiConnected(this)){
-			if(isConnectToEdu()){
-				if(!isNetConnected()){
-					result = true;
-				}else{
-					messageText.setText(getStr(R.string.login_successed));
-				}
-			}else{
-				messageText.setText("未连接到CMCC-EDU...");
-			}
+		if(isWifiConnected(context) && isConnectToEdu()){
+			return true;
 		}else{
 			messageText.setText("未连接到CMCC-EDU...");
+			return false;
 		}
-		return result;
 	}
-	
+
 	class MyHandler extends Handler{
 
 		public MyHandler(Looper looper){
@@ -116,21 +126,29 @@ public class AutoEduActivity extends Activity {
 				break;				
 			}
 		}
-		
+
 	}
-	
+
 	private String getStr(int id){
 		return context.getResources().getString(id);
 	}
-	
-	@Override
-	public void onWindowFocusChanged(boolean hasFocus)
-	{
-	   if(hasFocus){  
-		  if(isStartLogin()){
-        	  loginInThread = new LoginInThread(myHandler);
-        	  loginInThread.start();
-          }
-	   }
+
+
+	public static void saveValueInSp(Context ctx, List<String> list){
+		SharedPreferences sp = ctx.getSharedPreferences(SP_NAME, MODE_PRIVATE);
+		SharedPreferences.Editor editor = sp.edit();
+		editor.putString(SSID, list.get(0));
+		editor.putString(WLANACNAME, list.get(1));
+		editor.putString(WLANUSERIP, list.get(2));
+		editor.commit();
+	}
+
+	public static List<String> getPostParasList(Context ctx){
+		List<String> list = new ArrayList<String>();
+		SharedPreferences sp = ctx.getSharedPreferences(SP_NAME, MODE_PRIVATE);
+		list.add(sp.getString(SSID, ""));
+		list.add(sp.getString(WLANACNAME, ""));
+		list.add(sp.getString(WLANUSERIP, ""));
+		return list;
 	}
 }
--- src/com/withparadox2/autoedu/LoginInThread.java
+++ src/com/withparadox2/autoedu/LoginInThread.java
@@ -20,6 +20,7 @@ import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 
+import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
 
@@ -28,100 +29,132 @@ public class LoginInThread extends Thread{
 	private DefaultHttpClient httpClient;
 	private Message msg;
 	private Handler myHandler;
-	
-	public LoginInThread(Handler handler){
+	private Context ctx;
+	private int whichKindFlag;
+
+	public LoginInThread(Handler handler, Context ctx, int whichKindFlag){
 		this.myHandler = handler;
 		httpClient = new DefaultHttpClient();
+		this.ctx = ctx;
+		this.whichKindFlag = whichKindFlag;
 	}
-	
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		onStart();
+		switch (whichKindFlag) {
+		case AutoEduActivity.NORMAL_LOGIN:
+			normalLogin();
+			break;
+		case AutoEduActivity.FORCE_LOGIN:
+			forceLogin();
+			break;
+		}
 	}
-	
-	private void onStart(){
+
+
+	private void normalLogin(){
 		sendMyMessage(AutoEduActivity.START_LOGIN);
 		Header header = getLocationHeader(); 
 		if(header == null){
 			sendMyMessage(AutoEduActivity.NET_OK);
 		}else{
-			postLogin(header);
+			String locationHeaderValue = header.getValue().replace("-EDU", "520");
+			postLogin(getLoginParasList(locationHeaderValue));
 		}
 	}
-	
-	
+
+
 	public static Header getLocationHeader(){
 		HttpResponse response = null;
 		Header locationHeader = null;
-        DefaultHttpClient httpClient = new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet("http://www.baidu.com");
-        HttpParams httpParams = new BasicHttpParams();
-        httpParams.setParameter("http.protocol.handle-redirects", false); 
-        httpGet.setParams(httpParams);
-        try {
-            response = httpClient.execute(httpGet);
-        	locationHeader = response.getFirstHeader("Location");
-        } catch (ClientProtocolException e) {
-        // TODO Auto-generated catch block
-        	e.printStackTrace();
-        } catch (IOException e) {
-        // TODO Auto-generated catch block
-        	e.printStackTrace();
-        }
-        return locationHeader;
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet("http://www.baidu.com");
+		HttpParams httpParams = new BasicHttpParams();
+		httpParams.setParameter("http.protocol.handle-redirects", false); 
+		httpGet.setParams(httpParams);
+		try {
+			response = httpClient.execute(httpGet);
+			locationHeader = response.getFirstHeader("Location");
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return locationHeader;
 	}
 
-	private ArrayList<String> getLoginParas(String url){
+	/**
+	 * 返回一个包含三个post参数的列表，SSID，用户ip，用户名
+	 * @param url 通过访问baidu跳转得到的网址
+	 */
+	private ArrayList<String> getLoginParasList(String url){
 		Pattern pattern = Pattern.compile("=(.+?)&");
-        Matcher matcher = pattern.matcher(url);
-        ArrayList<String> list = new ArrayList<String>();
-        while (matcher.find()) {
-                 list.add(matcher.group(1));
-        }
+		Matcher matcher = pattern.matcher(url);
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("CMCC520");
+		while (matcher.find()) {
+			list.add(matcher.group(1));
+		}
+		AutoEduActivity.saveValueInSp(ctx, list);
 		return list; 
 	}	
-	
-	private void postLogin(Header locationHeader){
-		if(locationHeader != null){
-    		System.out.println("location=" + locationHeader.getValue());
-    		String locationHeaderValue = locationHeader.getValue().replace("-EDU", "520");
-    		HttpResponse response;
-    		ArrayList<String> list = getLoginParas(locationHeaderValue);
-    		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
-            nameValuePairs.add(new BasicNameValuePair("ssid", "CMCC520"));
-            nameValuePairs.add(new BasicNameValuePair("wlanacname", list.get(0)));
-            nameValuePairs.add(new BasicNameValuePair("wlanuserip", list.get(1)));
-            HttpPost httpPost = new HttpPost("http://120.202.164.10:8080/portal/servlets/BusinessLoginServlet");
-            try {
-				httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
-	            response = httpClient.execute(httpPost);
-	            String html = EntityUtils.toString(response.getEntity());
-	            if(isLoginSuccess(html)){
-	            	sendMyMessage(AutoEduActivity.LOGIN_SUCCESSED);
-	            }else{
-	            	sendMyMessage(AutoEduActivity.LOGIN_FAILED);
-	            }
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
+	/**
+	 *如果强制刷新失败，可能是用户ip变化了，因此需要通过<br>
+	 *normalLogin()来更新ip，重新提交一遍
+	 */
+	private void forceLogin(){
+		List<String> list  = AutoEduActivity.getPostParasList(ctx);
+		sendMyMessage(AutoEduActivity.START_LOGIN);
+		if(!postLogin(list)){
+			normalLogin();
 		}
 	}
-	
+
+	private boolean postLogin(List<String> list){
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+
+		nameValuePairs.add(new BasicNameValuePair("ssid", list.get(0)));
+		nameValuePairs.add(new BasicNameValuePair("wlanacname", list.get(1)));
+		nameValuePairs.add(new BasicNameValuePair("wlanuserip", list.get(2)));
+		HttpPost httpPost = new HttpPost("http://120.202.164.10:8080/portal/servlets/BusinessLoginServlet");
+		try {
+			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+			HttpResponse response = httpClient.execute(httpPost);
+			String html = EntityUtils.toString(response.getEntity());
+			System.out.println(html);
+			if(isLoginSuccess(html)){
+				sendMyMessage(AutoEduActivity.LOGIN_SUCCESSED);
+				return true;
+			}else{
+				sendMyMessage(AutoEduActivity.LOGIN_FAILED);
+				return false;
+			}
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+
+
 	private void sendMyMessage(int arg){
 		msg = myHandler.obtainMessage();
 		msg.arg1 = arg;
 		msg.sendToTarget();
 	}
-	
+
 	private boolean isLoginSuccess(String html){
-		return html.indexOf("用户登陆成功") != -1;
+		return html.contains("用户登录成功")||html.contains("返回码: 3022");
 	}
 }
